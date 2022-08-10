2bb8c490dd6b5a86dd4251c561a4e73dc692c3ce
Emeka Agara <emekaagara@gmail.com>
2022-08-10 11:44:59 +0100

    card done

--- src/App.js
+++ src/App.js
@@ -16,12 +16,16 @@ function App() {
           <img src={User} alt="user" className="h-9 w-9 object-cover rounded-full"/>
         </div>
         <div className="card mt-9">
-          <div>Marcellous Nwankwo</div>
-          <div>50% Credit Rating</div>
-          <div>ID12 **** 3456 7890</div>
-          <div>
-            <span>N1,200,000</span>
-            <img src={NisaLogo} alt="Nisa" className="h-4"/>
+          <div className="relative p-5 text-white">
+            <div className="text-lg">Marcellous Nwankwo</div>
+            <div className="mt-10 space-y-2">
+              <div>50% Credit Rating</div>
+              <div>ID12 **** 3456 7890</div>
+              <div className="flex justify-between relative">
+                <span>N1,200,000</span>
+                <img src={NisaLogo} alt="Nisa" className="h-4"/>
+              </div>
+            </div>
           </div>
         </div>
       </section>
